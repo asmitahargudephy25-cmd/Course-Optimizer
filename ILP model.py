a8f53b9edc123feae3e0676e34fb5963a5b17b1e
asmitahargudephy25-cmd <asmita.hargude.phy25@itbhu.ac.in>
2025-12-02 09:52:32 +0000

    built model and decision variables and added first hard constraint

--- ILP model.py
+++ ILP model.py
@@ -1,1 +1,23 @@
-print(4)
+courses_list = ["math", "cs", "physics", "calc1", "calc2"]
+semesters_list = [1,2,3,4,5,6,7,8]
+from ortools.sat.python import cp_model 
+
+#building model
+model = cp_model.CpModel()
+
+#creating decision variables(each variable has a domain{0,1})
+x = {}
+for c in courses_list:
+    for s in semesters_list:
+        x[('c','s')] = model.new_bool_var(f"x_{c}_{s}")
+
+#Hard Constraint 1(each course must be taken atmost once)
+for c in courses_list:
+    model.add(sum(x[('c','s')] for s in semesters_list) <= 1)
+   
+                  
+
+
+
+
+
