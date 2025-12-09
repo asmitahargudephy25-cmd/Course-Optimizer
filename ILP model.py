6c5187cc03d6ff00dbdbcde1e165be9155c9787c
asmitahargudephy25-cmd <asmita.hargude.phy25@itbhu.ac.in>
2025-12-09 11:27:14 +0000

    factored in major specific requirements

--- ILP model.py
+++ ILP model.py
@@ -2,7 +2,6 @@ import pandas as pd
 df = pd.read_csv("course_catalog.csv")
 courses_list = list(df["course_name"])
 semesters_list = [1,2,3,4,5,6,7,8]
-reqcourses_list = list(df[df['required_course'] == "YES"]["course_name"])
 credits_dict = df.set_index("course_name")["credits"].to_dict()
 
 max_credits = int(input("Enter max credits:"))
@@ -21,17 +20,22 @@ for c in courses_list:
 
 #Hard Constraint 1(each course must be taken atmost once)
 for c in courses_list:
-    model.add(sum(x[(c,s)] for s in semesters_list) <= 1)
+    model.Add(sum(x[(c,s)] for s in semesters_list) <= 1)
 
-#Hard Constraint 2(required courses must be taken exactly once)
-for courses in reqcourses_list:
-    model.add(sum(x[(c,s)] for s in semesters_list) == 1)
+#Hard Constraint 2(required courses for a particular major must be taken exactly once)
+major_pref = input("Your preferred major:")
+df2 = pd.read_csv("major_requirements.csv")
+
+req_string = df2[df2["major"] == major_pref]["required_courses"].values[0]
+for c in req_string.split("_") :
+    model.Add(sum(x[(c,s)] for s in semesters_list) == 1)
 
 #Hard Constraint 3(credit limits)
+total_credits = sum(x[(c,s)]*int(credits_dict[c]) for c in courses_list)
 for s in semesters_list:
-    model.add(sum(x[(c,s)]*credits_dict[c] for c in courses_list) <= max_credits)
+    model.Add(total_credits <= max_credits)
 for s in semesters_list:
-    model.add(sum(x[(c,s)]*credits_dict[c] for c in courses_list) >= min_credits)
+    model.Add(total_credits >= min_credits)
 
 #Hard Constraint 4(prerequisites)
 new_df = df[df["prerequisites"] != "NONE"]
@@ -42,7 +46,9 @@ for k in prereq_dict:
 for key in prereq_dict.keys():
     for p in prereq_dict[key]:
         for s in semesters_list:
-            model.add(x[key,s]<= sum(x[p,t] for t in range(1,s)))
+            model.Add(x[key,s]<= sum(x[p,t] for t in range(1,s)))
+
+
 
 
 #solver
@@ -56,6 +62,6 @@ if result in (cp_model.OPTIMAL, cp_model.FEASIBLE):
     for s in semesters_list:
         taken = [c for c in courses_list if solver.Value(x[(c, s)]) == 1]
         if taken:
-            print(f"Semester {s+1}: {taken}")
+            print(f"Semester {s}: {taken}")
 else:
     print("No feasible schedule found.")
